73954e66b8b116723c5787a8b4fbfd179f3aa04f
Persovt <46276626+Persovt@users.noreply.github.com>
2020-08-12 17:59:51 +0300

    third commit

--- docs/js/menu-burger.js
+++ docs/js/menu-burger.js
@@ -1,11 +1,11 @@
 /*burger burger-active */
 const burger = document.querySelector('.burger');
 const burgerActive = document.querySelector(".burger-active");
-const navBar    =document.querySelector(".navbar");
+const navBar = document.querySelector(".navbar");
 const body = document.querySelector("body")
-const brandname     =document.querySelector(".brandname");
-export const menuBurgerInit = () =>{
-    const menuToggle = () =>{
+const brandname = document.querySelector(".brandname");
+export const menuBurgerInit = () => {
+    const menuToggle = () => {
         burger.classList.toggle("burger-active");
         body.classList.toggle("fixed");
         navBar.classList.toggle("navbar-active");
--- docs/js/script.js
+++ docs/js/script.js
@@ -1,5 +1,5 @@
-import {sliderInit} from "./slider.js"
-import {menuBurgerInit} from "./menu-burger.js"
+import { sliderInit } from "./slider.js"
+import { menuBurgerInit } from "./menu-burger.js"
 sliderInit();
 menuBurgerInit();
 /* 
@@ -10,6 +10,7 @@ menuBurgerInit();
 .statistic__happyClient 
 */
 
+
 const statisticCiricleFirst = $(".statistic__ciricleFirst");
 const statisticHappyClient = $(".statistic__happyClient");
 const statisticComplitedProjects = $(".statistic__complitedProjects ");
@@ -17,32 +18,41 @@ const statisticBestshots = $(".statistic__bestshots");
 const statisticCiricleSecond = $(".statistic__ciricleSecond");
 const statisticCoffesmade = $(".statistic__coffesmade");
 const statisticWinningawards = $(".statistic__winningawards");
-const line          =  document.getElementsByTagName('line')[0];
-
+const line0 = document.querySelector('.line-0').getElementsByTagName('line')[0];
+const line1 = document.querySelector('.line-1').getElementsByTagName('line')[0];
+const line2 = document.querySelector('.line-2').getElementsByTagName('line')[0];
+const line3 = document.querySelector('.line-3').getElementsByTagName('line')[0];
+const line4 = document.querySelector('.line-4').getElementsByTagName('line')[0];
+const line5 = document.querySelector('.line-5').getElementsByTagName('line')[0];
 const statistic = document.querySelector(".statistic")
 
-let linePaint = (firstObject,secondObject) => {
-    let line = 0;
-    
-
-      let firstPos =  $(firstObject).position();
-      let secondPos = $(secondObject).position();
-      let posFirstObjectTop = firstPos.top + ($(firstObject).height() * 0.5)
-      let posFirstObjectLeft = firstPos.left + ($(firstObject).width() * 0.5)
-      let posSecondObjectTop = secondPos.top + ($(secondObject).height() * 0.5)
-      let posSecondObjectLeft = secondPos.left + ($(secondObject).width()* 0.5)
-    line = '<svg class="line"><line x1="'+ posFirstObjectLeft +'" y1="'+ posFirstObjectTop +'" x2="'+ posSecondObjectLeft +'" y2="'+ posSecondObjectTop +'" /></svg>'
-    $(line).prependTo(".statistic");
-        
-
+let linePaint = (firstObject, secondObject, linenumber) => {
+  let firstPos = $(firstObject).position();
+  let secondPos = $(secondObject).position();
+  let posFirstObjectTop = firstPos.top + ($(firstObject).height() * 0.5)
+  let posFirstObjectLeft = firstPos.left + ($(firstObject).width() * 0.5)
+  let posSecondObjectTop = secondPos.top + ($(secondObject).height() * 0.5)
+  let posSecondObjectLeft = secondPos.left + ($(secondObject).width() * 0.5)
+  linenumber.setAttribute('x1', posFirstObjectLeft);
+  linenumber.setAttribute('y1', posFirstObjectTop);
+  linenumber.setAttribute('x2', posSecondObjectLeft);
+  linenumber.setAttribute('y2', posSecondObjectTop);
 }
 
-linePaint(statisticCiricleFirst,statisticHappyClient);
-linePaint(statisticHappyClient,statisticComplitedProjects);
-linePaint(statisticBestshots,statisticCiricleSecond);
-linePaint(statisticBestshots,statisticCoffesmade);
-linePaint(statisticComplitedProjects,statisticBestshots);
-linePaint(statisticBestshots,statisticWinningawards);
+window.onresize = function(event) {
+  linePaint(statisticCiricleFirst, statisticHappyClient, line0);
+  linePaint(statisticHappyClient, statisticComplitedProjects , line1);
+  linePaint(statisticBestshots, statisticCiricleSecond, line2);
+  linePaint(statisticBestshots, statisticCoffesmade, line3);
+  linePaint(statisticComplitedProjects, statisticBestshots, line4);
+  linePaint(statisticBestshots, statisticWinningawards, line5);
+}
+linePaint(statisticCiricleFirst, statisticHappyClient, line0);
+linePaint(statisticHappyClient, statisticComplitedProjects , line1);
+linePaint(statisticBestshots, statisticCiricleSecond, line2);
+linePaint(statisticBestshots, statisticCoffesmade, line3);
+linePaint(statisticComplitedProjects, statisticBestshots, line4);
+linePaint(statisticBestshots, statisticWinningawards, line5);
 
 
 
